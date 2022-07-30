c6ef440865bc6478e24ffab7a1291def197a53ee
Shann Wei Yeh <weiwei.sw.yeh@gmail.com>
2022-07-30 23:52:57 +0800

    configs and install dependencies

--- config.sh
+++ config.sh
@@ -1,7 +1,7 @@
 #!bin/bash
 
-# requires qemu
-sudo apt install virtual-machine-manager
+# install dependencies
+sudo apt install -y thunar rofi kitty awesome variety virtual-machine-manager
 # fix qemu network 'default' not started (by enabling autostart)
 sudo virsh net-autostart default
 # check result
@@ -9,9 +9,6 @@ sudo virsh net-list --all
 
 # setup bluetooth to connect keyboard on boot
 sudo sh -c "echo 'FastConnectable=true' >> /etc/bluetooth/main.conf"
-
-# install Thunar (file explorer)
-sudo apt install -y variety thunar rofi kitty awesome
 
 # 150% scale on 4K (basically looks like 2K)
 xrandr --output `xrandr | grep " connected"|cut -f1 -d" "` --mode 3840x2160 --scale 0.75x0.75
