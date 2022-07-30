0f55a1446e1288f5f1fab6b46587530f996350f9
Shann Wei Yeh <weiwei.sw.yeh@gmail.com>
2022-07-30 23:50:40 +0800

    configs and install dependencies

--- config.sh
+++ config.sh
@@ -9,3 +9,11 @@ sudo virsh net-list --all
 
 # setup bluetooth to connect keyboard on boot
 sudo sh -c "echo 'FastConnectable=true' >> /etc/bluetooth/main.conf"
+
+# install Thunar (file explorer)
+sudo apt install -y variety thunar rofi kitty awesome
+
+# 150% scale on 4K (basically looks like 2K)
+xrandr --output `xrandr | grep " connected"|cut -f1 -d" "` --mode 3840x2160 --scale 0.75x0.75
+
+cp -r .config/* ~/.config
